ca1a5a388882e0e768efda48006dcb5e3cfd28dd
zassoulin <106088208+zassoulin@users.noreply.github.com>
2023-10-08 20:41:08 +0300

    working on creating a simple bot which only prints messages

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from telegramBot import TelegramBot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    import yaml
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with open('config.yaml') as config_file:
+        config = yaml.safe_load(config_file)
+
+    api_token = config.get('telegram_api_token')
+
+    if api_token is None:
+        raise ValueError('Telegram API token not found in the configuration file')
+
+    telegramBot = TelegramBot(api_token)
---
+++ telegramBot.py
@@ -0,0 +1,24 @@
+import telebot
+
+
+class TelegramBot:
+    def __init__(self, api_token):
+        self.bot = telebot.TeleBot(api_token)
+        self.group_ids = []  # List to store the group IDs this bot is a member of
+        self._register_handlers()
+
+    def _register_handlers(self):
+        @self.bot.message_handler(func=lambda message: True)
+        def handle_messages(message):
+            chat_id = message.chat.id
+            message_text = message.text
+            print(f"Received message from chat ID {chat_id}: {message_text}")
+    def join_group(self, group_id):
+        try:
+            self.bot.get_chat(group_id)  # Check if the bot can access the group
+            self.group_ids.append(group_id)
+            print(f"Bot has joined group with ID {group_id}")
+        except telebot.apihelper.ApiTelegramException as e:
+            print(f"Failed to join group with ID {group_id}: {e}")
+    def start_polling(self):
+        self.bot.polling(none_stop=True)
